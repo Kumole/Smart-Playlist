e961c0c30e09c625e385dc26bcbd108dcbdf79f8
Amirjjf <113022031+Amirjjf@users.noreply.github.com>
2023-12-09 15:21:28 +0200

    Update Recommender.py

--- Recommender.py
+++ Recommender.py
@@ -2,32 +2,24 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
-
 import warnings
+
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv("dataset\dataset.csv")
-df.drop(columns='Unnamed: 0',inplace=True)
+df = pd.read_csv("dataset/dataset.csv")
+df.drop(columns='Unnamed: 0', inplace=True)
 
-dfYear = pd.read_csv("data\data.csv")
-dfYear = dfYear[['id','year']]
+dfYear = pd.read_csv("data/data.csv")
+dfYear = dfYear[['id', 'year']]
 dfYear['track_id'] = dfYear['id']
-dfYear.drop(columns='id',inplace=True)
+dfYear.drop(columns='id', inplace=True)
 
-df = pd.merge(df,dfYear,on='track_id')
+df = pd.merge(df, dfYear, on='track_id')
 
-df[df.duplicated('track_id')==True]
+df[df.duplicated('track_id') == True]
 
-df[df['track_id']=='6Vc5wAMmXdKIAM7WUoEb7N']
-
-
-xtab_song = pd.crosstab(
-    df['track_id'], 
-    df['track_genre']
-)
-
-xtab_song = xtab_song*2
-
+xtab_song = pd.crosstab(df['track_id'], df['track_genre'])
+xtab_song = xtab_song * 2
 
 dfDistinct = df.drop_duplicates('track_id')
 dfDistinct = dfDistinct.sort_values('track_id')
@@ -36,8 +28,9 @@ dfDistinct = dfDistinct.reset_index(drop=True)
 xtab_song.reset_index(inplace=True)
 data_encoded = pd.concat([dfDistinct, xtab_song], axis=1)
 
+data_encoded['track_name_normalized'] = data_encoded['track_name'].str.lower().str.strip()
 
-numerical_features = ['explicit', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence','year']
+numerical_features = ['explicit', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'year']
 scaler = MinMaxScaler()
 data_encoded[numerical_features] = scaler.fit_transform(data_encoded[numerical_features])
 
@@ -45,12 +38,12 @@ calculatied_features = numerical_features + list(xtab_song.drop(columns='track_i
 
 cosine_sim = cosine_similarity(data_encoded[calculatied_features], data_encoded[calculatied_features])
 
+def get_recommendations(title, N=10):
+    indices = pd.Series(data_encoded.index, index=data_encoded['track_name_normalized']).drop_duplicates()
 
-def get_recommendations(title, N=5):
-    indices = pd.Series(data_encoded.index, index=data_encoded['track_name']).drop_duplicates()
-
+    title_normalized = title.lower().strip()
     try:
-        idx = indices[title]
+        idx = indices[title_normalized]
         try:
             len(idx)
             temp = 2
@@ -58,11 +51,12 @@ def get_recommendations(title, N=5):
             temp = 1
     except KeyError:
         return "Song not found in the dataset."
-    
+
     if temp == 2:
-        idx = indices[title][0]
+        idx = indices[title_normalized][0]
     else:
-        idx = indices[title]
+        idx = indices[title_normalized]
+
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:N+1]
@@ -76,7 +70,7 @@ def get_recommendations(title, N=5):
     
     return recommended_list
 
-recommended_songs = get_recommendations("Time", N=10)
+recommended_songs = get_recommendations("7 rings", N=10)
 if isinstance(recommended_songs, str):
     print(recommended_songs)
 else:
@@ -87,4 +81,3 @@ else:
         print(f"Album: {song['album_name']}")
         print(f"Similarity Score: {song['similarity_score']:.2f}")
         print()
-
